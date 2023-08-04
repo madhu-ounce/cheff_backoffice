6169bfe5f22fe7de964a0ed65b350c99ecb767f7
madhu-ounce <madhukarreddy062@gmail.com>
2023-08-04 10:57:22 +0530

    env added

--- src/components/profile.js
+++ src/components/profile.js
@@ -3,7 +3,7 @@ const ProfileComponent = () => {
     const porfile = useSelector((state) =>  state.logInfo )
     return (
         <div className="w_500">
-            <table className="table_1">
+            <table className="table_1 ">
                 <tr>
                     <td> Name </td>
                     <td> {porfile.firstName} </td>
